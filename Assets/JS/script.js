b4afb2f6d32773f3e23a2aff0a8d49328a6f9c86
ylfernando <ylfernando1@gmail.com>
2021-02-02 11:03:12 -0300

    Menu mobile feito

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,5 +1,9 @@
 $(document).ready(() => {
     $('.menu-mobile').on('click', () => {
-        var menu = $('.ul-header');
+        var menu = $('.menu-header-mobile');
+        if(menu) {
+        	menu.css('display', 'block');
+        }
+        else {menu.css('display', 'none');}
     });
 });
