fea4b96ee6fac2c1b16a13ebe36ff446bba00843
ylfernando <ylfernando1@gmail.com>
2021-02-03 10:16:30 -0300

    Menu mobile concluido

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,9 +1,9 @@
-$(document).ready(() => {
-    $('.menu-mobile').on('click', () => {
-        var menu = $('.menu-header-mobile');
-        if(menu) {
-        	menu.css('display', 'block');
-        }
-        else {menu.css('display', 'none');}
-    });
-});
+function menuMobile() {
+	var x = document.querySelector('.menu-header-mobile');
+
+	if(x.style.display === 'block') {
+		x.style.display = 'none';
+	} else {
+		x.style.display = 'block';
+	}
+}
